c1e3a406892bbcdf23fda348d54eb13dbe687e1e
Emma Nolan <emmanolan@Emmas-Mac-mini.local>
2024-04-20 11:24:36 +0100

    updated about text section

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <h1>ABOUT</h1>
         <div className="about-section">
           <div className="about-info">
-            <div>
+            <div className="about-text">
               <h2>
                 HEY I&apos;M EMMA, <br />
                 <span className="green">
@@ -15,21 +15,15 @@ export default function About() {
                 </span>
               </h2>
               <p>
-                As an enthusiastic Junior Full-Stack Developer with a keen eye
-                for detail, I am thrilled to embark on crafting inventive,
-                intuitive web applications. While I may be at the beginning of
-                my career journey, I am earnestly committed to expanding my
-                skills and evolving as a developer.
-                <br />
-                <br />
+                I am originally from Ireland, where I grew up on a small farm.
                 Previously I worked for an online nutritional supplement
-                retailer, where I first expereinced working with websites - both
+                retailer, where I first experienced working with websites - both
                 back-end and front-end. This is where my passion for developing
-                began, and I haven&apos;t looked back since! I began my journey
-                by jumping into different online coding resources and
-                self-taught myslef html and css. Following this, as I really
-                enjoyed it, I decided to take the next step in my tech career
-                and join an intensive full-stack bootcamp.
+                all started. I began my journey by jumping into different online
+                coding resources and self-taught myself html and css. Following
+                this I decided to take the next step in my tech career and join
+                an intensive full-stack bootcamp, and I haven&apos;t looked back
+                since!
               </p>
             </div>
             <div className="about-img">
